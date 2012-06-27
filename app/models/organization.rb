e0197f1d74f9564e17ae71838551d638175ad460
Leonel Santos <leonelsantos.me@gmail.com>
2012-06-27 11:14:21 -0500

    tweaks

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,18 @@
 class Organization < ActiveRecord::Base
   validates_presence_of :name
   has_many :invoices
+  
+  def formatted
+    a = "#{name} <br />" unless name.blank?
+    a = "#{a} #{address_one} <br />" unless address_one.blank?
+    a = "#{a} #{address_two} <br />" unless address_two.blank?
+    a = "#{a} #{city} " unless self.city.blank?
+    a = "#{a} #{state} " unless state.blank?
+    a = "#{a} #{postal_code} <br />" unless postal_code.blank?
+    a = "#{a} Phone: #{phone} <br />" unless phone.blank?
+    a = "#{a} Fax: #{fax} <br />" unless fax.blank?
+    a = "#{a} #{website} <br />" unless website.blank?
+    a = "#{a} RFC: #{rfc}" unless rfc.blank?
+    a.force_encoding('utf-8')
+  end
 end
